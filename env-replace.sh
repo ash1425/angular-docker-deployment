201238753e2a95bbd82df0fb9b98a2cb77f93c73
Ashay Thorat <ashayt@thoughtworks.com>
2021-01-04 17:45:57 +0100

    Better success message

--- env-replace.sh
+++ env-replace.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 sed -i "s/env-at-runtime/${CURRENT_ENV:?}/g" /usr/share/nginx/html/index.html
-echo "Success :: Set env and perform replacements"
+echo "Success :: Performed env specific initialization, starting NgInx"
